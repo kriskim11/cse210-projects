8308c23fc4bfd90f66f864fd337b1abe3ffda8c9
kriskim11 <kris.kimbrell11@gmail.com>
2023-11-02 21:25:30 -0700

    made a grade percentage using if,else if and else statment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine ("what is your grade percentage?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+        else if(percent >=60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your Grade is {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("better luck next time");
+        }
+
     }
+
 }
