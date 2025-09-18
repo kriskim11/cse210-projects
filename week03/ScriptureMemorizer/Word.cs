2510c04b2983ac81d368d44fff5cbbbd3213ed33
Kristopher Kimbrell <kris.kimbrell11@gmail.com>
2025-09-18 14:52:44 -0700

    project starter

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,27 @@
+using System;
+
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+
+    }
+    public void Hide()
+    {
+
+    }
+    public void Show()
+    {
+
+    }
+    public string GetDisplayText()
+    {
+        return "";
+
+    }
+
+}
